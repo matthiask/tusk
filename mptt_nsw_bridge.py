09f67b3cf4df01128e3cbca5f24a29296335ce6b
Matthias Kestenholz <mk@spinlock.ch>
2008-04-04 21:40:58 +0200

    Fixed a crasher bug in the mptt-nsw bridge
    Signed-off-by: Matthias Kestenholz <mk@spinlock.ch>

--- mptt_nsw_bridge.py
+++ mptt_nsw_bridge.py
@@ -1,4 +1,4 @@
-from django.db import connection, transaction
+from django.db import connection
 from django.utils.encoding import smart_unicode
 from django.utils import simplejson
 
@@ -42,7 +42,6 @@ def store_tree(cls, tree):
 			cls._meta.pk.column)
 
 		cursor.execute(sql % tuple(elem))
-	transaction.commit()
 
 def process_store_tree(cls, request):
 	structure = simplejson.loads(request.POST.get('nested-sortable-widget'))
